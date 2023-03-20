ba68742e18ed9c6fc2d7db2e4c5021a83eacaa72
Liony <gui.prouchandy@gmail.com>
2023-03-20 20:54:16 +0100

    intégration de la page de la lecture vidéo

--- src/Controller/StreamingSeasonController.php
+++ src/Controller/StreamingSeasonController.php
@@ -8,8 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StreamingSeasonController extends AbstractController
 {
-    #[Route('/streaming/season', name: 'streaming-season')]
-    public function index(): Response
+    #[Route('/streaming/season/anime', name: 'streaming_season_anime')]
+    public function anime(): Response
+    {
+        return $this->render('streaming_season/index.html.twig', [
+            'controller_name' => 'StreamingSeasonController',
+        ]);
+    }
+
+    #[Route('/streaming/season/movie', name: 'streaming_season_movie')]
+    public function movie(): Response
     {
         return $this->render('streaming_season/index.html.twig', [
             'controller_name' => 'StreamingSeasonController',
